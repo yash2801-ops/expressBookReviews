b6e8524d02e372f34adf336fd297b94de968657a
Yash Sadarangani <ysadarangani@gmail.com>
2024-08-30 05:36:31 -0400

    Task 2

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -17,8 +17,8 @@ public_users.get('/',function (req, res) {
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  let isbn = req.params.isbn
+  res.send(JSON.stringify(books[isbn]))
  });
   
 // Get book details based on author
