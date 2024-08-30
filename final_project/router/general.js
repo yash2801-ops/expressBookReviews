9d8fe69fb30dfd86015550ce104990eb6259b1f6
Yash Sadarangani <ysadarangani@gmail.com>
2024-08-30 06:28:02 -0400

    Task 3

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -23,9 +23,18 @@ public_users.get('/isbn/:isbn',function (req, res) {
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
-});
+    let author=req.params.author
+    let filteredBooks = []
+    for (let key in books) {
+        if(books[key].author===author){
+            filteredBooks.push(books[key])
+        }
+        
+    }
+    res.send(JSON.stringify(filteredBooks))
+}
+    
+);
 
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
