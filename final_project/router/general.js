a1be3a6ce7e5efe85d01faa30fba3de554402b29
Yash Sadarangani <ysadarangani@gmail.com>
2024-08-30 07:30:01 -0400

    Task 6

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -6,8 +6,17 @@ const public_users = express.Router();
 
 
 public_users.post("/register", (req,res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    const username = req.body.username;
+    const password = req.body.password;
+    // Check if both username and password are provided
+    if (username && password) {
+        users.push({"username": username, "password": password});
+        return res.status(200).json({message: "User successfully registered. Now you can login"});
+       
+    }
+    // Return error if username or password is missing
+    return res.status(404).json({message: "Unable to register user."});
+
 });
 
 // Get the book list available in the shop
